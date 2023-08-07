99334532b42ffad3bd5e026bb56cf61dc4a77dfa
Kevin Sasse <sassek70@gmail.com>
2023-08-06 23:54:36 -0400

    commit tree c09a95b344f5aa13f91943d99fb07ec92d2886aa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('%c HI', 'color: firebrick')
+//Get data
+fetch('https://dog.ceo/api/breeds/image/random/4')
+.then(res => res.json())
+.then(dogImagesArray => console.log(dogImagesArray))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getConfig = (verb, body) => {
+    const configurationObject = {
+    method: verb,
+    header: {
+        "Content-Type": "applications/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(body)
+}}
